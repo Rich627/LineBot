ad0dfeaaa45d4a0c2e11b27eb972db79d955f78f
Rich627 <103445968+Rich627@users.noreply.github.com>
2023-02-25 21:16:44 +0800

    done

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage
 )
 from deep_translator import GoogleTranslator
+import os
 
 app = Flask(__name__)
 
@@ -17,16 +18,21 @@ channel_secret = '1d2497c01a15d51f9550bf605b2c1fae'
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
 
+# 修正translate_text()函數
+def translate_text(source, target, text):
+    translator = GoogleTranslator(source=source, target=target)
+    return translator.translate(text)
+
 @app.route("/", methods=['POST'])
 def callback():
-    
+    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
-
+    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
-    
+    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
@@ -35,15 +41,12 @@ def callback():
 
     return 'OK'
 
-
-def translate_text(source, target, text):
-    translator = GoogleTranslator(source=source, target=target)
-    return translator.translate(text)
-
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    #if event.source.user_id == 'Udeadbeefdeadbeefdeadbeefdeadbeef':
+        #return 'OK'
     if event.message.text[:3] == "@翻英":
-        content = translate_text(source='zh-TW', target='en', text=event.message.text[3:])
+        content = translate_text(source='zh-tw', target='en', text=event.message.text[3:])
         message = TextSendMessage(text=content)
         line_bot_api.reply_message(event.reply_token, message)
     if event.message.text[:3] == "@翻中":
@@ -52,4 +55,5 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, message)
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port) 
