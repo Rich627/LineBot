6e382ec80885b3d5fa19750192acd714cc50af94
Rich627 <103445968+Rich627@users.noreply.github.com>
2023-03-01 18:19:05 +0800

    Done

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-!pip install line-bot-sdk
-!pip install deep_translator
-!pip install flask
-!pip install flask-ngrok
+# !pip install line-bot-sdk
+# !pip install deep_translator
+# !pip install flask
+# !pip install flask-ngrok
 from flask import Flask, request, abort
 from flask_ngrok import run_with_ngrok
 from linebot import (
@@ -20,24 +20,25 @@ app = Flask(__name__)
 
 channel_access_token='/tZ2uEIDxOMNlXK74nW70LYdCAYdLh5B8zDV+x3car+f+v+IhcY65be0QrzUxHvPJnyliDupV+LsrG38uGDKxY0E9mpx0KaorhocDMm9OLVlNWHQ0TObJBam+dEvBbI8f61GdJ0PWKr8JvpHV9v7CwdB04t89/1O/w1cDnyilFU='
 channel_secret = '1d2497c01a15d51f9550bf605b2c1fae'
+#先放入自己的channel_access_token和channel_secret
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
 
-# 修正translate_text()函數
+# 寫翻譯函數
 def translate_text(source, target, text):
     translator = GoogleTranslator(source=source, target=target)
     return translator.translate(text)
 
+# 設定server啟動時的指令
 @app.route("/", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-
-    # get request body as text
+    
+    # 請求內容
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
-    # handle webhook body
+    # 用handler處理請求內容
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
@@ -48,8 +49,6 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    #if event.source.user_id == 'Udeadbeefdeadbeefdeadbeefdeadbeef':
-        #return 'OK'
     if event.message.text[:3] == "@翻英":
         content = translate_text(source='zh-TW', target='en', text=event.message.text[3:])
         message = TextSendMessage(text=content)
